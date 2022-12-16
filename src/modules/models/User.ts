3fb707c504bac11795e62456bdafb401f4c150f4
nailson <nailson.ivs@codens.com.br>
2022-12-16 15:44:16 -0300

    feature(repositories)create Userrepository.ts created UserRepository and all methods with your implementations PLAYT-26

--- src/modules/models/User.ts
+++ src/modules/models/User.ts
@@ -2,9 +2,11 @@ import { v4 as uuidV4 } from "uuid";
 
 class User {
   id?: string;
-  nome: string;
+  name: string;
   login: string;
   password: string;
+  created_at: Date;
+  updated_at: Date;
 
   constructor() {
     if (!this.id) {
