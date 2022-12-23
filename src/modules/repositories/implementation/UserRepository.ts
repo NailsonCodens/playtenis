9a874790806af67dfcb92337af7ac0d779844595
nailson <nailson.ivs@codens.com.br>
2022-12-23 11:26:29 -0300

    refact(Files from users module) none

--- src/modules/repositories/implementation/UserRepository.ts
+++ src/modules/repositories/implementation/UserRepository.ts
@@ -1,4 +1,4 @@
-import { User } from "../../entities/User";
+import { User } from "../../entities/Users";
 import { IUserDTO } from "../../useCases/users/IUserDTO";
 import { IUserRepository } from "../IUserRepository";
 
