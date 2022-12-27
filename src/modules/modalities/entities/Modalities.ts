14ba3d1fae6159e691276b6644e5e6beb0942943
nailson <nailson.ivs@codens.com.br>
2022-12-27 11:42:23 -0300

    Feature(modules/modalities/entities/Modalities.ts) Create entity modalities
    PLAYT-36

--- src/modules/modalities/entities/Modalities.ts
+++ src/modules/modalities/entities/Modalities.ts
@@ -1,3 +1,17 @@
-class Modalities {}
+import { v4 as uuidV4 } from "uuid";
+
+class Modalities {
+  id: string;
+  name: string;
+  amount_players: string;
+  time: number;
+  status: string;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
 
 export { Modalities };
