1678fa703d344bf04589349de3bca6a0f560b4f4
nailson <nailson.ivs@codens.com.br>
2023-01-03 15:22:58 -0300

    Feature(/src/routes/modalities.routes.ts) Create list modalities
    Add route get from modalities
    PLAYT-37

--- src/routes/modalities.routes.ts
+++ src/routes/modalities.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
 import { CreateModalityControler } from "../modules/modalities/useCases/createModality/CreateModalityController";
+import { ListModalityController } from "../modules/modalities/useCases/ListModalities/ListModalityController";
 
 const modalitiesRoutes = Router();
 
 const createModalityController = new CreateModalityControler();
+const listModalityController = new ListModalityController();
 
 modalitiesRoutes.post("/", createModalityController.handle);
+modalitiesRoutes.get("/", listModalityController.handle);
 
 export { modalitiesRoutes };
