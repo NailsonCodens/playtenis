b02b4885f02f9988a4c89d845b70446b4a10e80c
nailson <nailson.ivs@codens.com.br>
2023-01-04 16:00:27 -0300

    Feature(src/database/migrations/1672857888372-CreateCourts.ts) Create migration table Courts Implement migration table court
    PLAYT40

---
+++ src/database/migrations/1672857888372-CreateCourts.ts
@@ -0,0 +1,36 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateCourts1672857888372 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "courts",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+          },
+          {
+            name: "court",
+            type: "varchar",
+          },
+          {
+            name: "status",
+            type: "varchar",
+          },
+          { name: "created_at", type: "timestamp", default: "now()" },
+          { name: "updated_at", type: "timestamp", default: "now()" },
+          {
+            name: "deleted_at",
+            type: "timestamp",
+            isNullable: true,
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("courts");
+  }
+}
