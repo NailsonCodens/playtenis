d8c47a668f099d4afe3f7db35916e460b98c03a8
nailson <nailson.ivs@codens.com.br>
2023-01-05 16:39:04 -0300

    Feature(src/modules/courts/useCases/deleteCourt) Create feature delete court Create delete court with typeorm. Is possibile to delete a court PLAYT-44

--- src/routes/courts.routes.ts
+++ src/routes/courts.routes.ts
@@ -1,14 +1,17 @@
 import { Router } from "express";
 
 import { CreateCourtController } from "../modules/courts/useCases/createCourt/CreateCourtController";
+import { DeleteCourtControler } from "../modules/courts/useCases/deleteCourt/DeleteCourtController";
 import { ListCourtController } from "../modules/courts/useCases/listCourts/ListCourtController";
 
 const courtRouter = Router();
 
 const createCourtController = new CreateCourtController();
 const listCourtController = new ListCourtController();
+const deleteCourtController = new DeleteCourtControler();
 
 courtRouter.post("/", createCourtController.handle);
 courtRouter.get("/", listCourtController.handle);
+courtRouter.delete("/", deleteCourtController.handle);
 
 export { courtRouter };
