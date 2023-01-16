a757718714531f299eb59f8d9dd6f411fe96244a
nailson <nailson.ivs@codens.com.br>
2023-01-16 15:28:55 -0300

    Test(src/modules/modalities/useCases/deleteModality) Create suit test delete modality feature
    PLAYT-70

---
+++ src/modules/modalities/useCases/deleteModality/DeleteModalityUseCase.spec.ts
@@ -0,0 +1,69 @@
+import { v4 as uuidV4 } from "uuid";
+
+import { ModalityRepositoryInMemory } from "@modules/modalities/repositories/in-memory/ModalityRepositoryInMemory";
+
+import { CreateModalityUseCase } from "../createModality/CreateModalityUseCase";
+import { DeleteModalityUseCase } from "./DeleteModalityUseCase";
+
+let deleteModalityUseCase: DeleteModalityUseCase;
+let createModalityUseCase: CreateModalityUseCase;
+let modalityRepositoryInMemory: ModalityRepositoryInMemory;
+
+describe("Suite Delete modality", () => {
+  beforeEach(() => {
+    modalityRepositoryInMemory = new ModalityRepositoryInMemory();
+    deleteModalityUseCase = new DeleteModalityUseCase(
+      modalityRepositoryInMemory
+    );
+
+    createModalityUseCase = new CreateModalityUseCase(
+      modalityRepositoryInMemory
+    );
+  });
+
+  it("Should be able to delete a modality", async () => {
+    let name = "Modality to delete";
+
+    await modalityRepositoryInMemory.create({
+      name,
+      amount_players: "4",
+      time: 90,
+      status: "ok",
+    });
+
+    name = "Modality deleted";
+
+    await modalityRepositoryInMemory.create({
+      name,
+      amount_players: "2",
+      time: 120,
+      status: "ok",
+    });
+
+    const modality = await modalityRepositoryInMemory.findByName(name);
+
+    const { id } = modality;
+    await deleteModalityUseCase.execute({ id });
+
+    const modalitiesExpected = await modalityRepositoryInMemory.list();
+
+    expect(modalitiesExpected).toEqual(expect.not.objectContaining(modality));
+  });
+
+  it("Should not be able to delete an non existent modality", async () => {
+    expect(async () => {
+      const name = "Modality to delete";
+
+      await modalityRepositoryInMemory.create({
+        name,
+        amount_players: "4",
+        time: 90,
+        status: "ok",
+      });
+
+      const id = uuidV4();
+
+      await deleteModalityUseCase.execute({ id });
+    });
+  });
+});
