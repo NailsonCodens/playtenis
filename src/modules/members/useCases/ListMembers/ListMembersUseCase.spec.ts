cab3546409ec3272130527291d7ec89d75e69f75
nailson <nailson.ivs@codens.com.br>
2023-01-18 15:25:43 -0300

    Feature(src/modules/members/useCases/ListMembers/ListMembersUseCase.spec.ts) continue implement list test
    PLAYT-76

--- src/modules/members/useCases/ListMembers/ListMembersUseCase.spec.ts
+++ src/modules/members/useCases/ListMembers/ListMembersUseCase.spec.ts
@@ -1,11 +1,29 @@
+import { MembersRepositoryInMemory } from "@modules/members/repositories/in-memory/MembersRepositoryInMemory";
+
 import { ListMembersUseCase } from "./ListMembersUseCase";
 
 let listMembersUseCase: ListMembersUseCase;
+let membersRepositoryInMemory: MembersRepositoryInMemory;
 
 describe("Suite list members use case", () => {
   beforeEach(() => {
-    listMembersUseCase = new ListMembersUseCase();
+    membersRepositoryInMemory = new MembersRepositoryInMemory();
+    listMembersUseCase = new ListMembersUseCase(membersRepositoryInMemory);
   });
 
-  it("Should be able to list members", async () => {});
+  it("Should be able to list members", async () => {
+    const name = "Membro 1";
+
+    await membersRepositoryInMemory.create({
+      name,
+      registration: "12345",
+      status: "ok",
+    });
+
+    const member = await membersRepositoryInMemory.findByName(name);
+
+    const users = await listMembersUseCase.execute();
+
+    expect(users).toEqual([member]);
+  });
 });
