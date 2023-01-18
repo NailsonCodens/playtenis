a5337abba719be974334b989327e76f84f04fce6
nailson <nailson.ivs@codens.com.br>
2023-01-18 15:38:32 -0300

    Refactor(src/modules/members/useCases/ListMembers/ListMembersUseCase.ts) Add tsrynge inject and injectable in list use case
    PLAYT-76

--- src/modules/members/useCases/ListMembers/ListMembersUseCase.ts
+++ src/modules/members/useCases/ListMembers/ListMembersUseCase.ts
@@ -1,8 +1,13 @@
+import { inject, injectable } from "tsyringe";
+
 import { Members } from "@modules/members/entities/Members";
 import { IMembersRepository } from "@modules/members/repositories/IMembersRepository";
 
+@injectable()
 class ListMembersUseCase {
-  constructor(private membersRepository: IMembersRepository) {}
+  constructor(
+    @inject("MembersRepository") private membersRepository: IMembersRepository
+  ) {}
 
   async execute(): Promise<Members[]> {
     const members = await this.membersRepository.list();
