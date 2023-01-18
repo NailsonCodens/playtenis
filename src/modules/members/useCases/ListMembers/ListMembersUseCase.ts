d6d84c86dcf6effbd7161997ada860d7a40fb402
nailson <nailson.ivs@codens.com.br>
2023-01-18 15:26:16 -0300

    Feature(src/modules/members/useCases/ListMembers/ListMembersUseCase.ts) continue implement useCase with method list members
    PLAYT-76

--- src/modules/members/useCases/ListMembers/ListMembersUseCase.ts
+++ src/modules/members/useCases/ListMembers/ListMembersUseCase.ts
@@ -1,7 +1,14 @@
 import { Members } from "@modules/members/entities/Members";
+import { IMembersRepository } from "@modules/members/repositories/IMembersRepository";
 
 class ListMembersUseCase {
-  async execute(): Promise<Members[]> {}
+  constructor(private membersRepository: IMembersRepository) {}
+
+  async execute(): Promise<Members[]> {
+    const members = await this.membersRepository.list();
+
+    return members;
+  }
 }
 
 export { ListMembersUseCase };
