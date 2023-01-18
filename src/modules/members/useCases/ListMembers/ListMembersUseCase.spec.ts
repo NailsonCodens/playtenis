9873138a65b67b7434316e7bf223440b57efb2e8
nailson <nailson.ivs@codens.com.br>
2023-01-18 15:15:13 -0300

    Feature(src/modules/members/useCases/ListMembers/ListMembersUseCase.spec.ts) create a usecase test
    PLAYT-76

---
+++ src/modules/members/useCases/ListMembers/ListMembersUseCase.spec.ts
@@ -0,0 +1,11 @@
+import { ListMembersUseCase } from "./ListMembersUseCase";
+
+let listMembersUseCase: ListMembersUseCase;
+
+describe("Suite list members use case", () => {
+  beforeEach(() => {
+    listMembersUseCase = new ListMembersUseCase();
+  });
+
+  it("Should be able to list members", async () => {});
+});
