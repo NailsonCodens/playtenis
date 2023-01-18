96833a713ccf07c1705ce6b1917e032f79230237
nailson <nailson.ivs@codens.com.br>
2023-01-18 15:36:32 -0300

    Feature(src/routes/members.routes.ts) Add route to list members

--- src/routes/members.routes.ts
+++ src/routes/members.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
 import { CreateMemberController } from "@modules/members/useCases/CreateMember/CreateMemberController";
+import { ListMembersController } from "@modules/members/useCases/ListMembers/ListMembersCotroller";
 
 const membersRoute = Router();
 
 const createMemberController = new CreateMemberController();
+const listMembersController = new ListMembersController();
 
 membersRoute.post("/", createMemberController.handle);
+membersRoute.get("/", listMembersController.handle);
 
 export { membersRoute };
