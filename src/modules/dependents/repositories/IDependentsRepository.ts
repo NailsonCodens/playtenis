6201cbc54ae3ebc16f6d74df64283d4ab7453a0a
nailson <nailson.ivs@codens.com.br>
2023-01-24 15:43:14 -0300

    Feature(src/modules/dependents/repositories/IDependentsRepository.ts) Implement listDependent feature
    PLAYT-80

--- src/modules/dependents/repositories/IDependentsRepository.ts
+++ src/modules/dependents/repositories/IDependentsRepository.ts
@@ -1,10 +1,11 @@
-import { Dependentes } from "@modules/dependents/entities/Dependents";
+import { Dependents } from "@modules/dependents/entities/Dependents";
 
 import { ICreateDependentDTO } from "../dtos/ICreateDependentDTO";
 
 interface IDependentesRepository {
   create(data: ICreateDependentDTO): Promise<void>;
-  findByName(name: string): Promise<Dependentes>;
+  findByName(name: string): Promise<Dependents>;
+  list(member_id: string): Promise<Dependents>;
 }
 
 export { IDependentesRepository };
