3ed91cc92faa30bf3be2a047db5fadcb2b7c2cc7
nailson <nailson.ivs@codens.com.br>
2023-01-26 08:00:08 -0300

    Feature(src/routes/depedents.routes.ts) add dele route
    PLAYT-82

--- src/routes/depedents.routes.ts
+++ src/routes/depedents.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import { CreateDependentController } from "@modules/dependents/useCases/createDependent/CreateDependentController";
+import { DeleteDependentController } from "@modules/dependents/useCases/DeleteDependent/DeleteDependentController";
 import { ListDependentsController } from "@modules/dependents/useCases/ListDependent/ListDependentsController";
 import { UpdateDepedentController } from "@modules/dependents/useCases/UpdateDependent/UpdateDependentController";
 
@@ -9,9 +10,11 @@ const dependentsRouter = Router();
 const createDependentController = new CreateDependentController();
 const updateDepedentController = new UpdateDepedentController();
 const listDependentController = new ListDependentsController();
+const deleteDependentController = new DeleteDependentController();
 
 dependentsRouter.post("/:member_id", createDependentController.handle);
 dependentsRouter.get("/:id", listDependentController.handle);
 dependentsRouter.put("/:id", updateDepedentController.handle);
+dependentsRouter.delete("/:id", deleteDependentController.handle);
 
 export { dependentsRouter };
