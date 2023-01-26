21ff17c89daa85ca76edf745c783d08d71c997ae
nailson <nailson.ivs@codens.com.br>
2023-01-26 08:28:11 -0300

    Feature(src/routes/members.routes.ts) Add delete route
    PLAYT-85

--- src/routes/members.routes.ts
+++ src/routes/members.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import { CreateMemberController } from "@modules/members/useCases/CreateMember/CreateMemberController";
+import { DeleteMemberController } from "@modules/members/useCases/DeleteMember/DeleteMemberController";
 import { ListMembersController } from "@modules/members/useCases/ListMembers/ListMembersCotroller";
 import { UpdateMemberController } from "@modules/members/useCases/UpdateMember/UpdateMemberController";
 
@@ -9,9 +10,11 @@ const membersRoute = Router();
 const createMemberController = new CreateMemberController();
 const listMembersController = new ListMembersController();
 const updateMemberController = new UpdateMemberController();
+const deleteMemberController = new DeleteMemberController();
 
 membersRoute.post("/", createMemberController.handle);
 membersRoute.get("/", listMembersController.handle);
 membersRoute.put("/:id", updateMemberController.handle);
+membersRoute.delete("/:id", deleteMemberController.handle);
 
 export { membersRoute };
