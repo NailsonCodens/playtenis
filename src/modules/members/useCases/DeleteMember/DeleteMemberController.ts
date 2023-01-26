0b6d6971df8474686497ba1ab24ed39fc444c852
nailson <nailson.ivs@codens.com.br>
2023-01-26 08:29:09 -0300

    Feature(src/modules/members/useCases/DeleteMember/) Create Dele UseCase, Controller and test
    PLAYT-85

---
+++ src/modules/members/useCases/DeleteMember/DeleteMemberController.ts
@@ -0,0 +1,20 @@
+import { Response, Request } from "express";
+import { container } from "tsyringe";
+
+import { DeleteMemberUseCase } from "./DeleteMemberUseCase";
+
+class DeleteMemberController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const deleteMemberUseCase = container.resolve(DeleteMemberUseCase);
+
+    await deleteMemberUseCase.execute(id);
+
+    return response
+      .status(201)
+      .json({ message: "Membro deletado com sucesso" });
+  }
+}
+
+export { DeleteMemberController };
