3d5145eb25952ecd6119d2d3ea8cac5ec560593b
nailson <nailson.ivs@codens.com.br>
2023-01-25 22:03:01 -0300

    Refactor(src/modules/dependents/useCases/ListDependent/ListDependentsUseCase.ts) add injects and injectable
    PLAYT-81

--- src/modules/dependents/useCases/ListDependent/ListDependentsUseCase.ts
+++ src/modules/dependents/useCases/ListDependent/ListDependentsUseCase.ts
@@ -1,7 +1,13 @@
-import { IDependentesRepository } from "@modules/dependents/repositories/IDependentsRepository";
+import { inject, injectable } from "tsyringe";
 
+import { IDependentsRepository } from "@modules/dependents/repositories/IDependentsRepository";
+
+@injectable()
 class ListDependentsUseCase {
-  constructor(private depedentsRepository: IDependentesRepository) {}
+  constructor(
+    @inject("DependentsRepository")
+    private depedentsRepository: IDependentsRepository
+  ) {}
 
   async execute(member_id: string) {
     const depedents = await this.depedentsRepository.list(member_id);
