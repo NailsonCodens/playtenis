393f9760f45ebf76569f8a360d3d8f7cfd68d514
nailson <nailson.ivs@codens.com.br>
2023-01-25 22:01:31 -0300

    Refactor(src/modules/dependents/repositories/in-memory/DependentsRepositoryInMemory.ts)
    PLAYT-81

--- src/modules/dependents/repositories/in-memory/DependentsRepositoryInMemory.ts
+++ src/modules/dependents/repositories/in-memory/DependentsRepositoryInMemory.ts
@@ -1,9 +1,9 @@
 import { ICreateDependentDTO } from "@modules/dependents/dtos/ICreateDependentDTO";
 import { Dependents } from "@modules/dependents/entities/Dependents";
 
-import { IDependentesRepository } from "../IDependentsRepository";
+import { IDependentsRepository } from "../IDependentsRepository";
 
-class DependentsRepositoryInMemory implements IDependentesRepository {
+class DependentsRepositoryInMemory implements IDependentsRepository {
   private dependents: Dependents[] = [];
 
   async create({ member_id, name }: ICreateDependentDTO): Promise<void> {
@@ -25,12 +25,40 @@ class DependentsRepositoryInMemory implements IDependentesRepository {
     return dependent;
   }
 
+  async findById(id: string): Promise<Dependents> {
+    const dependent = this.dependents.find((dependent) => dependent.id === id);
+
+    return dependent;
+  }
+
   async list(member_id: string): Promise<Dependents[]> {
     const dependents = this.dependents.filter(
       (dependent) => dependent.member_id === member_id
     );
 
     return dependents;
+  }
+
+  async update({
+    name,
+    member_id,
+    id,
+  }: ICreateDependentDTO): Promise<Dependents> {
+    const depedentIndex = this.dependents.findIndex(
+      (dependent) => dependent.id === id
+    );
+
+    const dependent = new Dependents();
+
+    Object.assign(dependent, {
+      id,
+      name,
+      member_id,
+    });
+
+    this.dependents[depedentIndex] = dependent;
+
+    return dependent;
   }
 }
 
