59028f21f014476de71022caee43ab87030f8c76
nailson <nailson.ivs@codens.com.br>
2023-01-26 17:15:39 -0300

    Feature(src/modules/games/dtos/ICreateGameDTO.ts) Create GameDTO
    PLAYT-86

--- src/modules/games/dtos/ICreateGameDTO.ts
+++ src/modules/games/dtos/ICreateGameDTO.ts
@@ -1,10 +1,9 @@
 interface ICreateGameDTO {
   court_id: string;
   modality_id: string;
-  modality_time: string;
-  start_time_game: string;
-  end_time_game: string;
-  date_game: Date;
+  modality_time: number;
+  start_time_game: Date;
+  end_time_game: Date;
 }
 
 export { ICreateGameDTO };
