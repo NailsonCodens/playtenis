b8e8b131d88466a01777bca1139986825892d2f2
nailson <nailson.ivs@codens.com.br>
2023-01-28 12:57:12 -0300

    Refactor(src/modules/games/useCases/createPlayersInGame) implement code, should be able to save players of the game
    PLAYT-88

--- src/modules/games/useCases/createPlayersInGame/CreatePlayersInGameUseCase.ts
+++ src/modules/games/useCases/createPlayersInGame/CreatePlayersInGameUseCase.ts
@@ -1,7 +1,56 @@
-class CreatePlayersInGameUseCase {
-  constructor() {}
+import { inject, injectable } from "tsyringe";
 
-  async execute(player_id: string, game_id: string): Promise<void> {}
+import { AppError } from "@errors/AppError";
+import { IGamesRepository } from "@modules/games/repositories/IGamesRepository";
+import { MembersRepository } from "@modules/members/repositories/implementation/MembersRepository";
+import { IModalitiesRepository } from "@modules/modalities/repositories/IModalitiesRepository";
+
+@injectable()
+class CreatePlayersInGameUseCase {
+  constructor(
+    @inject("GamesRepository") private gamesRepository: IGamesRepository,
+    @inject("MembersRepository") private membersRepository: MembersRepository,
+    @inject("ModalitiesRespository")
+    private modalitiesRepository: IModalitiesRepository
+  ) {}
+
+  async execute(player_ids: string[], game_id: string): Promise<void> {
+    const gameExists = await this.gamesRepository.findById(game_id);
+
+    if (!gameExists) {
+      throw new AppError("Este jogo não existe");
+    }
+
+    const modalityGame = await this.modalitiesRepository.findById(
+      gameExists.modality_id
+    );
+
+    const amountPlayers = player_ids.length;
+    const amoutPlayersAllowed =
+      modalityGame.amount_players as unknown as number;
+
+    if (amountPlayers > amoutPlayersAllowed) {
+      throw new AppError(
+        `Esta modalidade permite apenas ${amoutPlayersAllowed} jogadores`
+      );
+    }
+
+    if (amountPlayers < amoutPlayersAllowed) {
+      throw new AppError(
+        `Esta modalidade permite ${amoutPlayersAllowed} jogadores, por favor adicione outro jogador`
+      );
+    }
+    const players = await this.membersRepository.findByIds(player_ids);
+
+    gameExists.players = players;
+
+    const test = await this.gamesRepository.create(gameExists);
+
+    // verificar aqui se os mesmo jogadores de um jogo em andamento, estão tentando se cadastrar num novo jogo
+    // nao deve permitir que isto aconteça
+
+    console.log(test);
+  }
 }
 
 export { CreatePlayersInGameUseCase };
