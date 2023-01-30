3a2f1c92e6a36306594bf3d58909bb060dc28154
nailson <nailson.ivs@codens.com.br>
2023-01-30 10:58:48 -0300

    Feature(src/routes/courts.routes.ts)
    add route list all courts with games
    PLAYT-96

--- src/routes/courts.routes.ts
+++ src/routes/courts.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 import { CreateCourtController } from "@modules/courts/useCases/createCourt/CreateCourtController";
 import { DeleteCourtControler } from "@modules/courts/useCases/deleteCourt/DeleteCourtController";
+import { ListAllCourtsWithGamesController } from "@modules/courts/useCases/listAllCourtsWithGames/ListAllCourtsWithGamesController";
 import { ListCourtController } from "@modules/courts/useCases/listCourts/ListCourtController";
 import { UpdateCourtController } from "@modules/courts/useCases/updateCourt/UpdateCourtController";
 
@@ -9,12 +10,13 @@ const courtRouter = Router();
 
 const createCourtController = new CreateCourtController();
 const listCourtController = new ListCourtController();
+const listAllCourtsWithGamesController = new ListAllCourtsWithGamesController();
 const updateCourtController = new UpdateCourtController();
-
 const deleteCourtController = new DeleteCourtControler();
 
 courtRouter.post("/", createCourtController.handle);
 courtRouter.get("/", listCourtController.handle);
+courtRouter.get("/with-games", listAllCourtsWithGamesController.handle);
 courtRouter.put("/:id", updateCourtController.handle);
 courtRouter.delete("/:id", deleteCourtController.handle);
 
