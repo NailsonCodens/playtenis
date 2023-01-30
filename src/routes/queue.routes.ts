1c8619f0d764155109c7e304de42316a7bf38127
nailson <nailson.ivs@codens.com.br>
2023-01-30 17:04:49 -0300

    Feature(src/routes/queue.routes.ts) Create route list queue
    PLAYT-97

--- src/routes/queue.routes.ts
+++ src/routes/queue.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 
 import { AddGameInQueueController } from "@modules/queue/useCase/addGameInQueue/AddGameInQueueController";
+import { ListQueueController } from "@modules/queue/useCase/listQueue/ListQueueController";
 
 const queueRoutes = Router();
 
 const addGameInQueueController = new AddGameInQueueController();
+const listQueueController = new ListQueueController();
 
 queueRoutes.post("/", addGameInQueueController.handle);
-
+queueRoutes.get("/", listQueueController.handle);
 export { queueRoutes };
