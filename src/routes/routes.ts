3e11220b170b7050ab2b6f961e7e74d94173087d
nailson <nailson.ivs@codens.com.br>
2023-01-30 16:18:28 -0300

    Feature(src/routes/routes.ts) Add add queue route
    PLAYT-91

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -6,6 +6,7 @@ import { dependentsRouter } from "./depedents.routes";
 import { gamesRouter } from "./games.routes";
 import { membersRoute } from "./members.routes";
 import { modalitiesRoutes } from "./modalities.routes";
+import { queueRoutes } from "./queue.routes";
 import { userRoute } from "./users.routes";
 
 const generalRoutes = Router();
@@ -17,5 +18,6 @@ generalRoutes.use("/coachs", coachsRoutes);
 generalRoutes.use("/members", membersRoute);
 generalRoutes.use("/dependents", dependentsRouter);
 generalRoutes.use("/games", gamesRouter);
+generalRoutes.use("/queue", queueRoutes);
 
 export default generalRoutes;
