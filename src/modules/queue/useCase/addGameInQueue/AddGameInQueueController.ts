43c519467599c5becbe361d2d75f321360c6efa9
nailson <nailson.ivs@codens.com.br>
2023-01-30 16:17:20 -0300

    Feature(src/modules/queue/useCase/addGameInQueue/AddGameInQueueController.ts)
    implement
    
    PLAYT-91

--- src/modules/queue/useCase/addGameInQueue/AddGameInQueueController.ts
+++ src/modules/queue/useCase/addGameInQueue/AddGameInQueueController.ts
@@ -5,9 +5,16 @@ import { AddGameInQueueUseCase } from "./AddGameInQueueUseCase";
 
 class AddGameInQueueController {
   async handle(request: Request, response: Response) {
+    const { court_id, modality_id, players } = request.body;
+
     const addGameInQueueUseCase = container.resolve(AddGameInQueueUseCase);
 
-    addGameInQueueUseCase.execute();
+    await addGameInQueueUseCase.execute({ court_id, modality_id, players });
+
+    return response.json({
+      message:
+        "Jogadores adicionados a fila de espera para entrar na próxima quadra livre",
+    });
   }
 }
 
