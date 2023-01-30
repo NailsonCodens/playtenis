cb794753d46fc16aea0867a82e63b153b186e487
nailson <nailson.ivs@codens.com.br>
2023-01-30 11:48:38 -0300

    Feature(src/modules/dependents/entities/Dependents.ts) Add Relatioship with players_id and members id PLAYT-94

--- src/modules/dependents/entities/Dependents.ts
+++ src/modules/dependents/entities/Dependents.ts
@@ -2,10 +2,15 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToOne,
   PrimaryColumn,
   UpdateDateColumn,
 } from "typeorm";
 import { v4 as uuidV4 } from "uuid";
+
+import { Members } from "@modules/members/entities/Members";
 
 @Entity("dependents")
 class Dependents {
@@ -15,8 +20,22 @@ class Dependents {
   @Column("varchar")
   player_id: string;
 
+  @OneToOne(() => Members, (member) => member.id)
+  @JoinColumn({
+    name: "player_id",
+    referencedColumnName: "id",
+  })
+  player: Members;
+
   @Column("varchar")
   member_id: string;
+
+  @ManyToOne(() => Members, (member) => member.dependents)
+  @JoinColumn({
+    name: "member_id",
+    referencedColumnName: "id",
+  })
+  member: Members;
 
   @CreateDateColumn()
   created_at: Date;
