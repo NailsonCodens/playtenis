b5f93f57eb4e2bd8119df0047e85f9d521225db2
nailson <nailson.ivs@codens.com.br>
2023-02-09 17:50:37 -0300

    Feature(src/routes/coachs.routes.ts) Create list coach by id route
    PLAYT-104

--- src/routes/coachs.routes.ts
+++ src/routes/coachs.routes.ts
@@ -2,17 +2,20 @@ import { Router } from "express";
 
 import { CreateCoachController } from "@modules/coachs/useCases/createCoach/CreateCoachController";
 import { DeleteCoachController } from "@modules/coachs/useCases/deleteCoach/DeleteCoachController";
+import { ListCoachByIdController } from "@modules/coachs/useCases/listCoachById/ListCoachByIdController";
 import { ListCoachController } from "@modules/coachs/useCases/listCoachs/ListCoachController";
 import { UpdateCoachController } from "@modules/coachs/useCases/updateCoach/UpdateCoachController";
 
 const coachsRoutes = Router();
 
 const listCoachController = new ListCoachController();
+const listCoachByIdController = new ListCoachByIdController();
 const createCoachController = new CreateCoachController();
 const updateCoachController = new UpdateCoachController();
 const deleteCoachController = new DeleteCoachController();
 
 coachsRoutes.get("/", listCoachController.handle);
+coachsRoutes.get("/:id", listCoachByIdController.handle);
 coachsRoutes.post("/", createCoachController.handle);
 coachsRoutes.put("/:id", updateCoachController.handle);
 coachsRoutes.delete("/:id", deleteCoachController.handle);
