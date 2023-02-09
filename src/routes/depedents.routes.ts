9f8d3d46428e04e29e35f6d6caaa707739ed67c9
nailson <nailson.ivs@codens.com.br>
2023-02-09 18:29:34 -0300

    Feature(src/routes/depedents.routes.ts)
    add dependent by id route
    PLAYT-106

--- src/routes/depedents.routes.ts
+++ src/routes/depedents.routes.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { CreateDependentController } from "@modules/dependents/useCases/createDependent/CreateDependentController";
 import { DeleteDependentController } from "@modules/dependents/useCases/DeleteDependent/DeleteDependentController";
 import { ListDependentsController } from "@modules/dependents/useCases/ListDependent/ListDependentsController";
+import { ListDependentByIdController } from "@modules/dependents/useCases/listDependentById/ListDependentByIdController";
 import { UpdateDepedentController } from "@modules/dependents/useCases/UpdateDependent/UpdateDependentController";
 
 const dependentsRouter = Router();
@@ -10,10 +11,12 @@ const dependentsRouter = Router();
 const createDependentController = new CreateDependentController();
 const updateDepedentController = new UpdateDepedentController();
 const listDependentController = new ListDependentsController();
+const listDependentByIdController = new ListDependentByIdController();
 const deleteDependentController = new DeleteDependentController();
 
 dependentsRouter.post("/:member_id", createDependentController.handle);
 dependentsRouter.get("/:member_id", listDependentController.handle);
+dependentsRouter.get("/dependent/:id", listDependentByIdController.handle);
 dependentsRouter.put("/:id", updateDepedentController.handle);
 dependentsRouter.delete("/:id", deleteDependentController.handle);
 
