a57e669abceaf0bea95c0e3992fb8af04c4b9dd1
nailson <nailson.ivs@codens.com.br>
2023-02-09 17:41:04 -0300

    Feature(src/routes/courts.routes.ts) Add list court by id route
    PLAYT-107

--- src/routes/courts.routes.ts
+++ src/routes/courts.routes.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { CreateCourtController } from "@modules/courts/useCases/createCourt/CreateCourtController";
 import { DeleteCourtControler } from "@modules/courts/useCases/deleteCourt/DeleteCourtController";
 import { ListAllCourtsWithGamesController } from "@modules/courts/useCases/listAllCourtsWithGames/ListAllCourtsWithGamesController";
+import { ListCourtByIdController } from "@modules/courts/useCases/listCourtById/ListCourtByIdController";
 import { ListCourtController } from "@modules/courts/useCases/listCourts/ListCourtController";
 import { UpdateCourtController } from "@modules/courts/useCases/updateCourt/UpdateCourtController";
 
@@ -10,12 +11,14 @@ const courtRouter = Router();
 
 const createCourtController = new CreateCourtController();
 const listCourtController = new ListCourtController();
+const listCourtByidController = new ListCourtByIdController();
 const listAllCourtsWithGamesController = new ListAllCourtsWithGamesController();
 const updateCourtController = new UpdateCourtController();
 const deleteCourtController = new DeleteCourtControler();
 
 courtRouter.post("/", createCourtController.handle);
 courtRouter.get("/", listCourtController.handle);
+courtRouter.get("/:id", listCourtByidController.handle);
 courtRouter.get("/with-games", listAllCourtsWithGamesController.handle);
 courtRouter.put("/:id", updateCourtController.handle);
 courtRouter.delete("/:id", deleteCourtController.handle);
