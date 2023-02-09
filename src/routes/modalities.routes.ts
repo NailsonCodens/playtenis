73b10b17649a640f38a78658a5dd277b41f01a62
nailson <nailson.ivs@codens.com.br>
2023-02-09 17:31:45 -0300

    Feature(src/routes/modalities.routes.ts) Create list omdality by id route
    PLAYT-102

--- src/routes/modalities.routes.ts
+++ src/routes/modalities.routes.ts
@@ -3,17 +3,20 @@ import { Router } from "express";
 import { CreateModalityController } from "@modules/modalities/useCases/createModality/CreateModalityController";
 import { DeleteModalityController } from "@modules/modalities/useCases/deleteModality/DeleteModalityController";
 import { ListModalityController } from "@modules/modalities/useCases/listModalities/ListModalityController";
+import { ListModalityByIdController } from "@modules/modalities/useCases/listModalityById/ListModalityByIdController";
 import { UpdateModalityController } from "@modules/modalities/useCases/updateModality/UpdateModalityController";
 
 const modalitiesRoutes = Router();
 
 const createModalityController = new CreateModalityController();
 const listModalityController = new ListModalityController();
+const listModalityByIdController = new ListModalityByIdController();
 const updateModalityController = new UpdateModalityController();
 const deleteModalityController = new DeleteModalityController();
 
 modalitiesRoutes.post("/", createModalityController.handle);
 modalitiesRoutes.get("/", listModalityController.handle);
+modalitiesRoutes.get("/:id", listModalityByIdController.handle);
 modalitiesRoutes.put("/:id", updateModalityController.handle);
 modalitiesRoutes.delete("/:id", deleteModalityController.handle);
 export { modalitiesRoutes };
