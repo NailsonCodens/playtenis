86be916ec74b5869f4bba75a4cb10373d9f06c28
nailson <nailson.ivs@codens.com.br>
2023-02-09 18:05:00 -0300

    Feature(src/routes/members.routes.ts) Add list member by id route
    PLAYT-105

--- src/routes/members.routes.ts
+++ src/routes/members.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 import { CreateMemberController } from "@modules/members/useCases/CreateMember/CreateMemberController";
 import { DeleteMemberController } from "@modules/members/useCases/DeleteMember/DeleteMemberController";
+import { ListMemberByIdController } from "@modules/members/useCases/listMemberById/ListMemberByIdController";
 import { ListMembersController } from "@modules/members/useCases/ListMembers/ListMembersCotroller";
 import { UpdateMemberController } from "@modules/members/useCases/UpdateMember/UpdateMemberController";
 
@@ -9,11 +10,13 @@ const membersRoute = Router();
 
 const createMemberController = new CreateMemberController();
 const listMembersController = new ListMembersController();
+const listMemberByIdController = new ListMemberByIdController();
 const updateMemberController = new UpdateMemberController();
 const deleteMemberController = new DeleteMemberController();
 
 membersRoute.post("/", createMemberController.handle);
 membersRoute.get("/", listMembersController.handle);
+membersRoute.get("/:id", listMemberByIdController.handle);
 membersRoute.put("/:id", updateMemberController.handle);
 membersRoute.delete("/:id", deleteMemberController.handle);
 
